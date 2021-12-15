74451d444ca88bba7ee4954e72e926a21fdabc67
sasank174 <perumallasasank123@gmail.com>
2021-12-15 18:42:52 +0530

    commit

--- 30 Days of Code/Day_29_Bitwise_AND.py
+++ 30 Days of Code/Day_29_Bitwise_AND.py
@@ -1,6 +1,7 @@
 # Objective
 # Welcome to the last day! Today, we're discussing bitwise operations. Check out the Tutorial tab for learning materials and an instructional video!
 
+
 # Task
 # Given set . Find two integers,  and  (where ), from set  such that the value of  is the maximum possible and also less than a given integer, . In this case,  represents the bitwise AND operator.
 
